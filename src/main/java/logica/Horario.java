d73901172551ada686c0dc9b1519daf00ea705dc
Heiler <pheilerggomez@gmail.com>
2024-06-04 21:23:40 -0400

    carga inicial

---
+++ src/main/java/logica/Horario.java
@@ -0,0 +1,16 @@
+package logica;
+
+import gui.Login;
+
+
+public class Horario {
+
+    public static void main(String[] args) {
+        
+        
+        Login log = new Login();
+        log.setVisible(true);
+      
+    
+        }
+}
